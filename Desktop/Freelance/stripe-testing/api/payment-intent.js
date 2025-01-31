bf8a17e91179da06185e599ef5b1e52c16ad0f23
Maxime BOUVIER <129532676+Maxbouvierpro@users.noreply.github.com>
2025-01-31 19:24:35 +0800

    Update payment-intent.js

--- Desktop/Freelance/stripe-testing/api/payment-intent.js
+++ Desktop/Freelance/stripe-testing/api/payment-intent.js
@@ -6,12 +6,22 @@ export default async function handler(req, res) {
   }
 
   try {
-    const { amount } = req.body;
+    const { productId } = req.body;
 
+    // Vérification du produit envoyé
+    if (productId !== "prod_RgfebF9Wr8sIay") {
+      return res.status(400).json({ error: "Produit invalide" });
+    }
+
+    // Prix en centimes (56€)
+    const amount = 5600;
+
+    // Création du PaymentIntent
     const paymentIntent = await stripe.paymentIntents.create({
       amount,
       currency: "eur",
       payment_method_types: ["card"],
+      metadata: { productName: "Abonnement Mensuel - 56€" },
     });
 
     res.status(200).json({ clientSecret: paymentIntent.client_secret });
